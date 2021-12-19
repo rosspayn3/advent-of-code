dd7dc39cc5326293af04f3f401caef660f889163
Ross Payne <rosspayn3@gmail.com>
2021-12-19 05:12:32 -0600

    day 1 part 1

---
+++ 2021/Day 1/solution.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const readings = fs.readFileSync('input.txt', 'utf8').split('\r\n');
+
+// console.log(readings);
+
+let numLarger = 0;
+let prevReading = parseInt(readings[0])
+
+readings.forEach(reading => {
+    if(parseInt(reading) > prevReading){
+        numLarger++;
+    }
+    prevReading = reading;
+})
+
+console.log(numLarger)
