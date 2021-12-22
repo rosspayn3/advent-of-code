1a3d332f5443f3a6ea661b97e50ddb2bc8832ca0
Ross Payne <rosspayn3@gmail.com>
2021-12-21 19:40:40 -0600

    day 3 part 1 finished

---
+++ 2021/Day 3/p1.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+
+const readings = fs.readFileSync("input.txt", "utf8").split("\r\n");
+
+// console.log(readings);
+
+let values = new Map();
+let firstVal = readings[0];
+let gamma = [];
+let epsilon = [];
+let gammaDecimal = 0;
+let epsilonDecimal = 0;
+
+// **** initialize values map ****
+
+for (let index = 0; index < firstVal.length; index++) {
+    values.set(index, 0);
+}
+
+// console.log(values);
+
+// **** get most common bit value for each position the readings ****
+
+for (let index = 0; index < readings.length; index++) {
+    const reading = readings[index];
+    for (let index = 0; index < reading.length; index++) {
+        let numOnes = values.get(index);
+        const bit = reading[index];
+        if (bit == "1") {
+            values.set(index, ++numOnes);
+        }
+    }
+}
+
+// **** build gamma and epsilon binary strings ****
+
+values.forEach((value, key) => {
+    // console.log("position: ", key, "value: ", value);
+    if (value > readings.length / 2) {
+        gamma.push(1);
+        epsilon.push(0);
+    } else {
+        epsilon.push(1);
+        gamma.push(0);
+    }
+});
+
+// console.log("gamma binary: ", gamma, "epsilon binary: ", epsilon);
+
+// **** get decimal representation of gamma and epsilon binary strings ****
+
+let binaryValue = 1;
+
+for (let index = gamma.length - 1; index >= 0; index--) {
+    const gammaBit = gamma[index];
+    const epsilonBit = epsilon[index];
+
+    if (gammaBit == "1") {
+        gammaDecimal += binaryValue;
+    }
+    if (epsilonBit == "1") {
+        epsilonDecimal += binaryValue;
+    }
+
+    binaryValue = binaryValue * 2;
+}
+
+// console.log("decimal values: ", gammaDecimal, epsilonDecimal);
+
+// **** calculate power consumption of submarine ****
+
+let powerConsumption = gammaDecimal * epsilonDecimal;
+console.log(powerConsumption)
